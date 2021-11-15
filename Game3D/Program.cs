40f72d8ddcda82b3702225476ca6b29fa7d010b9
flo <pub2flo@gmail.com>
2021-11-15 22:09:05 +0100

    First Commit

---
+++ Game3D/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Game3D
+{
+    internal static class Program
+    {
+        
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("hello world");
+        }
+    }
+}
---
+++ Game3D/Vector.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Game3D
+{
+    public class Vector
+    {
+    }
+}
